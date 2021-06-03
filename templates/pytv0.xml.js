bc17b674edb9bca63466be798897c2942eff58ed
CHRIS DEV <chrisdev@ChrisCherryJr-MacBook-Pro-2.local>
2021-06-03 10:22:00 -0400

    first commit with assets for Apple Review, but need correct movie links

---
+++ templates/pytv0.xml.js
@@ -0,0 +1,71 @@
+var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+<document>
+<head>
+    <style>
+    .main {
+        background-color: #F9C74F;
+    }
+</style>
+</head>
+  <listTemplate class="main">
+    <banner>
+      <background>
+        <img src="${this.BASEURL}resources/py_temp_bannerver2_aTV.png" width="1920" height="360" />
+      </background>
+    </banner>
+    <list>
+      <section>
+        <header>
+          <title>tvOS Perpetual Yogi's Draft App</title>
+        </header>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/tvOS-xcode-font-size.mp4">
+          <title>Featuring Yogi Amanda.F</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/amanda_.png" width="555" height="400" />
+              <title>This is my Yoga Journey</title>
+              <subtitle>Certified 200hr SUP/Aerial Yoga YTT</subtitle>
+              <description>A mother of 3, a wife small business owner, and worked in the medical field since High School</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/tvOS-custom-view-focus.mp4">
+          <title>Featuring Jamekia.L</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/jamekia_yogapark_icon.png" width="857" height="482" />
+              <title>Certified 200hr RYT Chair/Aerial/Restorative</title>
+              <description>I am Jamekia Lumsden, the FlowerChild Yogi, here to be a guide and help you remember your truth through synchronizing breath with movement. We flow and you blossom. This is a safe haven to let all go.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/app-icon-photoshop.mp4">
+          <title>Featuring Yogi Wendy.L</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/wendycropicon_draft.png" width="857" height="482" />
+              <title>Certified 200hr RYT Chair/Aerial/Restorative</title>
+              <subtitle>I'm also trained in Trauma Sensitive Yoga</subtitle>
+              <description>I work with veterans with PTSD and substance abuse, when I'm not teaching yoga in the park, you will find with with my husband, and the rest of my wonderful family</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+         <header>
+          <title>...</title>
+        </header>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/tvOS-xcode-font-size.mp4">
+          <title>Featuring Christopher.C Jr</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/Chris_icon_.png" width="857" height="482" />
+              <title>Yogi Developer</title>
+              <subtitle>Still and always a student in Yoga and Learning Code</subtitle>
+              <description>Army Veteran tvOS Coder that uses yoga to escape his own self(for Real...)</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+      </section>
+    </list>
+  </listTemplate>
+</document>`
+}
---
+++ templates/pytv0_copy.xml.js
@@ -0,0 +1,71 @@
+var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
+<document>
+<head>
+    <style>
+    .main {
+        background-color: #F9DB6D;
+    }
+</style>
+</head>
+  <listTemplate class="main">
+    <banner>
+      <background>
+        <img src="${this.BASEURL}resources/py_temp_bannerver2_aTV.png" width="1920" height="320" />
+      </background>
+    </banner>
+    <list>
+      <section>
+        <header>
+          <title>Perpetual Yogi's Draft App</title>
+        </header>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/tvOS-xcode-font-size.mp4">
+          <title>Featuring Yogi Amanda.F</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/amanda_.png" width="555" height="400" />
+              <title>Certified 200hr SUP/Aerial Yoga YTT</title>
+              <subtitle>Her chakra is the Solar Plexus</subtitle>
+              <description>I am a mother of three, a wife small business owner, and I worked in the medical field since High School.  My hobbies are spending as much time possible with family, paddle boarding, yoga, reading, and traveling.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/tvOS-custom-view-focus.mp4">
+          <title>Featuring FlowerChild Yogi Jamekia.L</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/jamekia_yogapark_icon.png" width="555" height="400" />
+              <title>Certified 200hr RYT Chair/Aerial/Restorative</title>
+              <description>I am Jamekia Lumsden, the FlowerChild Yogi, here to be a guide and help you remember your truth through synchronizing breath with movement. We flow and you blossom. This is a safe haven to let it all go.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/app-icon-photoshop.mp4">
+          <title>Featuring Yogi Wendy.L</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/wendycropicon_draft.png" width="555" height="400" />
+              <title>Certified 200hr RYT Chair/Aerial/Restorative</title>
+              <subtitle>I'm also trained in Trauma Sensitive Yoga</subtitle>
+              <description>I work with veterans with PTSD and substance abuse, when I'm not teaching yoga in the park, you will find with with my husband, and the rest of my wonderful family.</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+         <header>
+          <title>...</title>
+        </header>
+        <listItemLockup vidUrl="https://s3-us-west-2.amazonaws.com/devslopesvideo/tvOS-xcode-font-size.mp4">
+          <title>Featuring yogi Christopher.C Jr</title>
+          <relatedContent>
+            <lockup>
+              <img src="${this.BASEURL}resources/Chris_icon_.png" width="555" height="400" />
+              <title>Certified 200hr YTT/Yogi Developer</title>
+              <subtitle>Still and always a student in Yoga and learning to code tvOS/AppleTV's</subtitle>
+              <description>Army Vet and tvOS developer that uses yoga to escape my own self(lol).</description>
+            </lockup>
+          </relatedContent>
+        </listItemLockup>
+      </section>
+    </list>
+  </listTemplate>
+</document>`
+}
